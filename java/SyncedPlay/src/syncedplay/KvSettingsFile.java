848aea0b52b50e236d289e450b8ff09c9b3e4669
Kevin Johnson <kevin@coolkev.com>
2013-10-25 14:53:02 -0400

    Fixed the code that each file uses to read the setting files.
    Removed the superclass SettingsFile which was just getting in the way and not doing very much to simplify things.

--- java/SyncedPlay/src/syncedplay/KvSettingsFile.java
+++ java/SyncedPlay/src/syncedplay/KvSettingsFile.java
@@ -10,44 +10,52 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 /** This class stores data about settings files.
  *
  * @author kevin
  */
-
 //TODO Work was pasued on this. Fix this file.
 
 /*
  * 
  * 
-re_startsection = re.compile(r"\[(.*)\]")
+re_startsection = re.compile()
 re_data = re.compile(r"\_(.*)=(.*)")
 re_generickeyvalue = re.compile(r"(.*)=(.*)")
 re_command = re.compile(r"\^(.*)=(.*)")
 re_comment = re.compile(r"#.*$")
 
  */
- abstract class KvSectionsFile extends SettingsFile {
+class KvSectionsFile {
 
-    Pattern genericKeyValue = Pattern.compile("(.*)=(.*)");
     HashMap<String, String> dictionary = new HashMap();
-    /** This initializer will read the file and generate the keyvalue dictionary based upon what it reads */
-    public KvSectionsFile(String filename) throws FileNotFoundException, IOException {
-        /*get a handle on the file */
-        
-        /* Read each line */
-    }
+    Pattern genericKeyValue = Pattern.compile("(.*)=(.*)");
     
-    private ReadData(BufferedReader br){
+    /** This initializer will read the file and generate the keyvalue dictionary based upon what it reads */
+    public KvSectionsFile(String filename) throws FileNotFoundException {
+        FileReader fileReader = new FileReader(new File(filename));
+        BufferedReader br = new BufferedReader(fileReader);
+        ReadData(br);
+    }
+
+    private void ReadData(BufferedReader br) {
         String line = null;
-        while ((line = br.readLine()) != null) {
-            /*For each line, read a key-value pair and store it*/
-            Matcher matches = genericKeyValue.matcher(line);
-            String key = matches.group(1);
-            String value = matches.group(2);
-            dictionary.put(key, value);
+        try {
+            while ((line = br.readLine()) != null) {
+                // For each line, read a key-value pair and store it
+                Matcher matches = genericKeyValue.matcher(line);
+                String key = matches.group(1);
+                String value = matches.group(2);
+                dictionary.put(key, value);
+            }
+        } catch (IOException e) {
+            Logger.getLogger(KvSectionsFile.class.getName()).log(Level.SEVERE, null, e);
+
         }
     }
 }
--- java/SyncedPlay/src/syncedplay/SectionedSettingsFile.java
+++ java/SyncedPlay/src/syncedplay/SectionedSettingsFile.java
@@ -5,30 +5,56 @@
 package syncedplay;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
  * @author kevin
  */
-public class SectionedSettingsFile extends SettingsFile {
+class SectionedSettingsFile {
+
+    HashMap<String, HashMap> dictionary = new HashMap();
+    Pattern genericKeyValue = Pattern.compile("(.*)=(.*)");
+    Pattern sectionStart = Pattern.compile("\\[(.*)\\]");
+    
     public SectionedSettingsFile(String filename) throws FileNotFoundException {
-      super(filename);
+        FileReader fileReader = new FileReader(new File(filename));
+        BufferedReader br = new BufferedReader(fileReader);
+        ReadData(br);
     }
-              
-    @Override
-    void ReadData(BufferedReader br){
+
+    private void ReadData(BufferedReader br) {
         String line = null;
+        HashMap<String, String> currentsection = null;
         try {
             while ((line = br.readLine()) != null) {
-                /*For each line, read a key-value pair and store it*/
-                Matcher matches = genericKeyValue.matcher(line);
-                String key = matches.group(1);
-                String value = matches.group(2);
-                dictionary.put(key, value);
+
+                Matcher startsection = sectionStart.matcher(line);
+                Matcher kvmatch = genericKeyValue.matcher(line);
+
+                // Check to see if it matches a start of section
+                if (startsection.find()) { //The line is the start of a section
+                    currentsection = new HashMap();
+                    dictionary.put(startsection.group(1), currentsection);
+
+                } else if (kvmatch.find()) { //The line is a kv pair
+
+                    String key = kvmatch.group(1);
+                    String value = kvmatch.group(2);
+
+                    currentsection.put(key, value);
+                }
             }
-        } catch (IOException e){}
+        } catch (IOException ex) {
+            Logger.getLogger(KvSectionsFile.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
